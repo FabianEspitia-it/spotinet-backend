f895dc55dd13e47330aca9276d5d25d5b572fa69
Fabián Espitia <fabianespitia370@gmail.com>
2024-10-03 21:49:55 -0500

    added arabic replace on netflix method

--- src/utils/netflix_methods.py
+++ src/utils/netflix_methods.py
@@ -47,7 +47,7 @@ def get_netflix_code_email(user_email: str, email_subject: str) -> str:
                                 encoding if encoding else "utf-8")
 
                         new_subject: str = subject.replace(
-                            " ", "").replace("RV:", "").replace("FW:", "")
+                            " ", "").replace("RV:", "").replace("FW:", "").replace("هدایت:", "")
 
                         print(new_subject)
                         print(email_subject)
@@ -83,91 +83,6 @@ def get_netflix_code_email(user_email: str, email_subject: str) -> str:
 
                             else:
                                 return 'No se encontró ningún link'
-
-                                """
-
-                                driver = webdriver.Chrome()
-
-                                driver.get(link)
-
-                                time.sleep(2)
-
-                                try:
-
-                                    email_element = WebDriverWait(driver, 5).until(
-                                        EC.presence_of_element_located(
-                                            (By.XPATH, "//*[@id=':r0:']"))
-                                    )
-
-                                    email_element.send_keys(user_email)
-
-                                    button = WebDriverWait(driver, 5).until(
-                                        EC.element_to_be_clickable(
-                                            (By.CSS_SELECTOR, 'button[data-uia="login-toggle-button"]'))
-                                    )
-
-                                    button.click()
-
-                                    button_two = WebDriverWait(driver, 5).until(
-                                        EC.element_to_be_clickable(
-                                            (By.CSS_SELECTOR, 'button[data-uia="login-submit-button"]'))
-                                    )
-
-                                    button_two.click()
-
-                                    time.sleep(7)
-
-                                    session_code = get_netflix_code_email(
-                                        user_email=user_email)
-
-                                    first_input_box = WebDriverWait(driver, 10).until(
-                                        EC.visibility_of_element_located(
-                                            (By.CSS_SELECTOR, 'input.default-ltr-cache-u0nsmb-Digit.ecrfx4d3'))
-                                    )
-
-                                    pyperclip.copy(session_code)
-
-                                    first_input_box.click()
-
-                                    action = ActionChains(driver)
-                                    action.key_down(Keys.CONTROL).send_keys(
-                                        'v').key_up(Keys.CONTROL).perform()
-
-                                    submit_button = WebDriverWait(driver, 5).until(
-                                        EC.element_to_be_clickable(
-                                            (By.CSS_SELECTOR, 'button[type="submit"]'))
-                                    )
-
-                                    submit_button.click()
-
-                                    time.sleep(2)
-                                except Exception as e:
-                                    pass
-
-                                if email_subject == "Tu código de acceso temporal de Netflix".replace(" ", ""):
-
-                                    code = WebDriverWait(driver, 5).until(
-                                        EC.presence_of_element_located(
-                                            (By.CSS_SELECTOR, '[data-uia="travel-verification-otp"]'))
-                                    )
-
-                                    return code.text
-
-                                else:
-
-                                    print("entramos")
-                                    button = WebDriverWait(driver, 5).until(
-                                        EC.element_to_be_clickable(
-                                            (By.CSS_SELECTOR, 'button[data-uia="set-primary-location-action"]'))
-                                    )
-
-                                    button.click()
-
-                                    time.sleep(2)
-
-                                    return "Process completed"
-
-                                """
 
                         else:
                             print("No se encontró el asunto del correo")
